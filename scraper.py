ddf1e7a8a783b4f8280cbec6f4481081ac19bfad
James Lawlor <jalawlor@tcd.ie>
2017-06-09 13:47:37 +0100

    cleaned up scraper.py

--- scraper.py
+++ scraper.py
@@ -25,22 +25,14 @@ song_list = [re.sub(r'\W+', '', x.lower()) for x in lang]
 song_url = "http://www.azlyrics.com/lyrics/aesoprock/"
 song_list = [(x,song_url + x + ".html") for x in song_list]
 
-<<<<<<< HEAD
 # iterate through song urls, write to text file ./lyrics/songname.txt
-=======
-# iterate through song urls, write to text file
->>>>>>> 7a2d221a7b0af188583c35d56089af77c49a9dd3
 with requests.Session() as s:
     s.headers.update(headers)
     for song_name, song_url in song_list:
         download = s.get(song_url)
         site= BeautifulSoup(download.text,"html.parser")
         for lyrics in site.find_all("div", {"class":""}):
-<<<<<<< HEAD
             with open("./lyrics/"+song_name+".txt","w") as fout:
-=======
-            with open(song_name+".txt","w") as fout:
->>>>>>> 7a2d221a7b0af188583c35d56089af77c49a9dd3
                 fout.write(lyrics.text)
             time.sleep(np.random.uniform(low=10.0, high=20.0, size=None))
         print(song_name)
